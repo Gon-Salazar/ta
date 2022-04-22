7a08ec647be7e6ab45f3f707accb32a68c47e862
Gon-Salazar <101957305+Gon-Salazar@users.noreply.github.com>
2022-04-21 20:53:49 -0500

    Add files via upload

--- tabla de amortirzación/Program.cs
+++ tabla de amortirzación/Program.cs
@@ -1,41 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace prestamo_01
+namespace Consola_Tabla_de_amortizacion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //recopilar informacion
+
+            float pago, Interes_pagado, Capital_pagado, Monto_del_prestamo, tasa_interes_anual, tasa_interes_mensual;
+            int  fila, Plazo, i;
+
             
-            Console.WriteLine("fecha");
-                 var currentDate = DateTime.Now ;
+            Console.WriteLine("fecha del prestamo");
+                var currentDate = DateTime.Now ;
             Console.WriteLine($"{currentDate:d} at {currentDate:t}!");
             Console.ReadKey (true);
+            Console.Write("Introduce el monto del prestamo: ");
+            float.TryParse(Console.ReadLine(), out Monto_del_prestamo);
+            Console.Write("Introduce la tasa de interes anual: ");
+            float.TryParse(Console.ReadLine(), out tasa_interes_anual);
+            Console.Write("Introduce el plazo en meses: ");
+            int.TryParse(Console.ReadLine(), out Plazo);
+            Console.Write("fecha de pago: ");
 
-            Console.WriteLine("ingrese el monto solicitado");
-            double Prestamo = double.Parse(Console.ReadLine());
+            //Calculo del interes mensual
+            tasa_interes_mensual = (tasa_interes_anual / 100) / 12;
 
 
-            Console.WriteLine("ingrese el porcentaje de interes ");
-            double interes = double.Parse(Console.ReadLine());
+            //Calculo de la cuota mensual
+            pago = tasa_interes_mensual + 1;
+            pago = (float)Math.Pow(pago, Plazo);
+            pago = pago - 1;
+            pago = tasa_interes_mensual / pago;
+            pago = tasa_interes_mensual + pago;
+            pago = Monto_del_prestamo * pago;
 
 
-            Console.WriteLine("ingrese plazo en meses a pagar");
-            int Tiempo = int.Parse(Console.ReadLine());
 
-            //aqui hago los calculos
-            interes = (Prestamo * interes) / 100;
+           
+            Console.WriteLine();
+            fila = 1;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" Numero de pago \t");
+            Console.Write("Pago \t\t");
+            Console.Write("Intereses Pagados \t\t");
+            Console.Write("Capital Pagado \t\t");
+            Console.Write("Monto del prestamo \t");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("\t0");
+            Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t{0}", Monto_del_prestamo);
 
-            double PrestamoTotal = Prestamo + interes;
 
-            double Cuotas = PrestamoTotal / Tiempo;
-            //ahora muestro lo pedido en pantalla
+            for (i = 1; i <= Plazo; i++)
+            {
 
-            Console.WriteLine("el monto del crédito es de: " + PrestamoTotal);
-            Console.WriteLine("la tasa a pagar es de: " + interes);
-            Console.WriteLine("el tiempo a pagar es de: " + Tiempo);
-            Console.WriteLine("la cuota mensual es de: " + Cuotas);
+             
+                Console.Write("\t{0}\t\t", fila);
+
+               
+                Console.Write("{0}\t",pago);
+
+               
+                Interes_pagado = tasa_interes_mensual * Monto_del_prestamo;
+                Console.Write("{0}\t\t",Interes_pagado);
+
+               
+                Capital_pagado = pago - Interes_pagado;
+                Console.Write("\t{0}\t",Capital_pagado);
+
+               
+                Monto_del_prestamo = Monto_del_prestamo - Capital_pagado;
+                Console.Write("\t{0}\t",Monto_del_prestamo);
+
+                fila = fila + 1;
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
         }
     }
 }
